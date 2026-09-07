1b3b3e7f241861e52d3aa1175e0dedd0295f200d
MauriceLiang <lywnbsb@163.com>
2026-09-07 17:53:07 +0800

    add

---
+++ day01/type-practice.py
@@ -0,0 +1,18 @@
+# 输出类型
+name = "梁渊玮"
+age = 20
+value = 99.99
+hasCar = None
+lst = [1,2,3]
+tp = (1,2,3)
+isMarried = False
+print(type(name))
+print(type(age))
+print(type(value))
+print(type(hasCar))
+print(type(lst))
+print(type(tp))
+print(type(isMarried))
+# 字符串转换
+str = "100"
+print(int(str) + 200)
