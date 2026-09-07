ab922094833fe7e7de61a5d234b4d2f464a508ad
MauriceLiang <lywnbsb@163.com>
2026-09-07 18:56:26 +0800

    输入输出练习

---
+++ day01/input_output.py
@@ -0,0 +1,10 @@
+#年龄练习
+name = input("请输入你的姓名：\n")
+age = input("请输入你的年龄：\n")
+print(f"大家好，我叫{name},我今年{age}岁了")
+#整数计算
+num1 = 9
+num2 = 3
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
